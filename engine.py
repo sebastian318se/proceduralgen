7e5fbbf0f872f0a47730ed87a404e880384da51b
sebastian318se <sebastianbferrari@gmailcom>
2026-01-24 01:20:43 -0500

    UI WIP

--- engine.py
+++ engine.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Optional
 import matplotlib.pyplot as plt
 import numpy
@@ -12,12 +12,13 @@ class TerrainEngine:
     periodX: int # Rougher or smoother terrain 
     periodY: int
 
-
     seed: int
     dimension: str #2D or 3D
 
     octaves: Optional[int] = 1
     water_level: Optional[float] = None
+
+    surface: Optional[object] = field(init = False, default = None)
 
     def generate(self):
 
@@ -32,16 +33,29 @@ class TerrainEngine:
 
         # Output 2D Terrain
         if self.dimension == "2D":
-            terrain_plot = plt.imshow(noise, cmap='terrain')
-
+            self.surface = plt.imshow(noise, cmap='terrain')
+            # return xy here
+            
         # Output 3D Terrain
         elif self.dimension == "3D":
             # Make three dimensional visualization using concentration as height
             x_coords, y_coords = numpy.meshgrid(numpy.arange(self.height), numpy.arange(self.width))
             fig = plt.figure()
             ax = fig.add_subplot(111, projection="3d")
-            terrain_plot = ax.plot_surface(x_coords, y_coords, noise, cmap='terrain', linewidth = 0)
+            self.surface = ax.plot_surface(x_coords, y_coords, noise, cmap='terrain', linewidth = 0)
+            # return xyz here
 
         else:
             raise NameError
-        plt.show()        
+        
+        return self.surface
+
+"""     
+TODO
+
+Make visual area shorter proportional to water level
+Make vertical walls not water whenever water level is active
+Make a seed generator (Way to save numpy.random.seed every new gen for export later).
+Make way to re-create maps
+GUI
+"""
--- generation.py
+++ generation.py
@@ -1,4 +1,57 @@
 import engine
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        
+        self.setWindowTitle("Galapo Terrain Engine")
+
+        # Container = Main Window Widget; Vertical (Box) Grid
+        container = QWidget()
+        self.setCentralWidget(container)
+        layout = QVBoxLayout(container)       
+
+        # Button Grid Over Graph View
+        labels = ["1", "2"]
+        bLayout = self.buttonGrid(labels)
+        layout.addWidget(bLayout)
+
+        # Main Window -- Place matplotlib terrain plot
+        plch = QLabel('Insert plt')
+        plch.setAlignment(Qt.AlignCenter)
+        layout.addWidget(plch)
+        
+        canvas = FigureCanvasQTAgg(fig)
+        canvas.draw_idle()
+        layout.addWidget(canvas)
+
+        genButton = QPushButton('Butoon')
+        layout.addWidget(genButton)
+
+        # Button Grid Under Graph View
+        labels = ["1", "2", "3", "4"] 
+        bLayout = self.buttonGrid(labels)
+        layout.addWidget(bLayout)
+       
+
+    def buttonGrid(self, labels, columns = 2):
+
+        container = QWidget()
+        layout = QGridLayout(container)
+
+        for i, text in enumerate(labels):
+            x = (i // columns) # Two labels per row
+            y = (i % columns) # Go down a row whenever remainder is 1
+            button = QPushButton(text)
+            layout.addWidget(button, x, y)
+
+        return container
 
 world = engine.TerrainEngine(
     height = 200,
@@ -11,10 +64,21 @@ world = engine.TerrainEngine(
     dimension = "3D",
     water_level= 0
     )
-world.generate()
-""" 
-Make visual area shorter proportionally to water level 
-Make a seed generator (Way to save numpy.random.seed every new gen for export later).
-Make way to re-create maps
-GUI
-"""
+fig = Figure()
+
+if world.dimension == "3D":
+    ax = fig.add_subplot(111, projection = "3d")
+else:
+    ax = fig.add_subplot(111)
+    
+# Break engine generate() into gen() and draw() to call directly from ui code and plot correctly. draw into qt axes
+# (Make axes passed as arguments so it works both as standalone and ui appl. w/o dupe code)
+# QT should know to update the plot because its axes are being used on the drawing code.
+# canvas.draw_idle() to update it
+
+app = QApplication()
+
+main_window = MainWindow()
+main_window.show()
+
+app.exec()
